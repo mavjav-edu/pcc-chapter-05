792b80835822755daefe8ee7ad9ad566b6ec3b99
Mavaddat Javid <mavaddat.javid@gmail.com>
2021-07-23 18:03:57 -0700

    update to pcc_2e

--- chapter_05/banned_users.py
+++ chapter_05/banned_users.py
@@ -2,4 +2,4 @@ banned_users = ['andrew', 'carolina', 'david']
 user = 'marie'
 
 if user not in banned_users:
-    print(user.title() + ", you can post a response if you wish.")
+    print(f"{user.title()}, you can post a response if you wish.")
--- chapter_05/toppings.py
+++ chapter_05/toppings.py
@@ -5,8 +5,8 @@ requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
 for requested_topping in requested_toppings:
     if requested_topping in available_toppings:
-        print("Adding " + requested_topping + ".")
+        print(f"Adding {requested_topping}.")
     else:
-        print("Sorry, we don't have " + requested_topping + ".")
+        print(f"Sorry, we don't have {requested_topping}.")
         
 print("\nFinished making your pizza!")
